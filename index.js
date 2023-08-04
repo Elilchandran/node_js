21bcebdee7b0c7754ae6becbb4066f5e452f0c91
Elilchandran <elilchandran@gmail.com>
2023-08-04 17:08:26 +0530

    till post and get

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app=express();
 
-const notes = [
+//middleware
+app.use(express.json());
+
+let notes = [
   { id: '1', title: 'hello', body: 'world', important: true },
   { id: '2', title: 'hello', body: 'world', important: true },
   { id: '3', title: 'hello', body: 'world', important: false },
@@ -11,14 +14,33 @@ const notes = [
   { id: '7', title: 'backend using node.js', body: 'Important note about backend', important: false }
 ];
 
-//setting end point:
-app.get('/', (req,res)=>{
-  res.send("api request"); 
+//end point for getting:
+app.get('/', (request,response) => {
+  response.send("api request");
 });
 //to get all the notes end point:
-app.get('/api/notes', (req,res)=>{
-  res.json(notes); 
+app.get('/api/notes', (request,response)=>{
+  //response.json(notes); 
+  response.status(200).json(notes);//when the new added data(id 8:veera cant be visible while fetching single resources)
 });
+//to create *new resource* based on request data(post request end point):
+app.post("/api/notes",(request, response) =>{
+  notes = notes.concat(request.body);
+  response.status(201).json({message:'note created successfully'});
+});
+
+//fetches a *single resource* based on id:
+app.get("/api/notes/:id",(request, response) =>{
+const id=request.params.id;
+const note=notes.find(note => note.id == id);
+if(note){
+  response.status(200).json(note);
+}else{
+  response.status(404).json({message:'id does not found'});//or.end("id not exsit");//404 status means id not /data not found
+}
+//response.status(200).json(note);//returning if data avaiable(ex:1 to 7 id we can return data in postman)
+});
+
 
 
 const PORT =3001;
