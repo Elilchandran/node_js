a93aeaadea590d5f06440934d3b7109ac6fe4a57
Elilchandran <elilchandran@gmail.com>
2023-08-04 17:26:40 +0530

    Add date-time.txt file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app=express();
 
 //middleware
@@ -46,4 +46,81 @@ if(note){
 const PORT =3001;
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`);
+});*/
+
+/*const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3001; // You can use any port you prefer
+
+// Endpoint to create a text file with the current timestamp
+app.get('/api/create-file', (req, res) => {
+  const timestamp = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-');
+  const fileName = `${timestamp}.txt`;
+
+  fs.writeFile(`files/${fileName}`, timestamp, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to create the file' });
+    }
+
+    res.status(201).json({ message: 'File created successfully' });
+  });
+});
+
+// Endpoint to retrieve all text files in the 'files' folder
+app.get('/api/get-files', (req, res) => {
+  fs.readdir('files', (err, files) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read the folder' });
+    }
+
+    res.status(200).json({ files });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+*/
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3001; // 
+
+// Middleware
+app.use(express.json());
+
+// Endpoint to create a text file with the current timestamp
+app.get('/api/create-file', (req, res) => {
+  const currentTime = new Date().toISOString();
+  const fileName = 'date-time.txt';
+
+  fs.writeFile(fileName, currentTime, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return res.status(500).json({ error: 'Failed to create the file' });
+    }
+
+    console.log(`File '${fileName}' created successfully with the content:`, currentTime);
+    res.status(201).json({ message: 'File created successfully' });
+  });
+});
+
+// Endpoint to retrieve all text files in the 'files' folder
+app.get('/api/get-files', (req, res) => {
+  fs.readdir('files', (err, files) => {
+    if (err) {
+      console.error('Error reading the folder:', err);
+      return res.status(500).json({ error: 'Failed to read the folder' });
+    }
+
+    res.status(200).json({ files });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
